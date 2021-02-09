e75da141b9939d5ecdac053663df692e819edce2
Matthew Cook <matthewcook13987@gmail.com>
2021-02-09 20:49:31 +0000

    Created spinner2 which refactors spinner1 to remove repeated code and instead loop continuously over the same function.

---
+++ spinner2.js
@@ -0,0 +1,47 @@
+
+const sentence = "|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\\n";
+let timeOut = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(`\r${char}`);
+  }, timeOut);
+  timeOut += 150;
+}
+
+
+// setTimeout(() => {
+//   process.stdout.write("\r|   ");
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write("\r/   ");
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write("\r-   ");
+// }, 500);
+
+// setTimeout(() => {
+//   process.stdout.write("\r\\   ");
+// }, 700);
+
+// setTimeout(() => {
+//   process.stdout.write("\r|   ");
+// }, 900);
+
+// setTimeout(() => {
+//   process.stdout.write("\r/   ");
+// }, 1100);
+
+// setTimeout(() => {
+//   process.stdout.write("\r-   ");
+// }, 1300);
+
+// setTimeout(() => {
+//   process.stdout.write("\r\\   ");
+// }, 1500);
+
+// setTimeout(() => {
+//   process.stdout.write("\r|   ");
+// }, 1700);
